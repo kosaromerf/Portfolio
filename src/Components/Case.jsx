a6fb858eae657a765e2a68c6f83e89bf9453b6b9
kosaromerf <kosaromerf@gmail.com>
2024-05-08 20:23:45 +0300

    Add: Projects and Case components

--- src/Components/Case.jsx
+++ src/Components/Case.jsx
@@ -8,16 +8,26 @@ const Case = ({ prewiew, name, detail, techs, codeLink, prewiewLink }) => {
   ));
 
   return (
-    <div>
+    <div className="case">
       <section className="casePrewiew">{prewiew}</section>
-      <section className="caseDetail">
+      <section className="caseInfo">
         <h3 className="caseName">{name}</h3>
         <p className="caseDetail">{detail}</p>
-        <div className="">{...labels}</div>
-        <a href={codeLink} target="_blank" rel="noopener noreferrer">
+        <div className="labels">{labels}</div>
+        <a
+          className="codeLink"
+          href={codeLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           GitHub
         </a>
-        <a href={prewiewLink} target="_blank" rel="noopener noreferrer">
+        <a
+          className="prewiewLink"
+          href={prewiewLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Live Review
         </a>
       </section>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,11 +4,34 @@ import Case from "./Case";
 const Projects = () => {
   return (
     <div id="projects">
+      <h2>Projects</h2>
       <Case
         prewiew={"#"}
         name={"Spice Shop"}
-        detail={"Spice Shop Details"}
-        techs={["a", "b", "c"]}
+        detail={
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo, id sapiente ratione? Veniam. id sapiente ratione? Veniam. Details"
+        }
+        techs={["HTML", "CSS", "JavaScript", "React"]}
+        codeLink={"#"}
+        prewiewLink={"#"}
+      />
+      <Case
+        prewiew={"#"}
+        name={"Battleship Game"}
+        detail={
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo, id sapiente ratione? Veniam. id sapiente ratione? Veniam. Details"
+        }
+        techs={["HTML", "CSS", "JavaScript", "React"]}
+        codeLink={"#"}
+        prewiewLink={"#"}
+      />
+      <Case
+        prewiew={"#"}
+        name={"Inventory Application"}
+        detail={
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ut velit minus eum ad accusamus nemo, id sapiente ratione? Veniam. id sapiente ratione? Veniam. Details"
+        }
+        techs={["HTML", "CSS", "JavaScript", "NodeJS"]}
         codeLink={"#"}
         prewiewLink={"#"}
       />
