2dc53473a8f96d0c38ac36855a6d3d474e6c77a8
kosaromerf <kosaromerf@gmail.com>
2024-05-10 23:25:09 +0300

    Add: Responsive Welcome Component

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -8,23 +8,35 @@ const Welcome = () => {
     animationDelay();
   });
   return (
-    <div className="typewriterContainer" id="welcome">
+    <div id="welcome">
       <img
         src={welcomeLight}
         alt="a illistruation of a penguan listening music and coding"
         id="welcomeMop"
       />
-      <h1 className="typewriter">Hi , I am Omer</h1>
-      <h2 className="typewriter">Designer and Frontend Developer</h2>
-      <ul>
-        <li className="typewriter">-Responsive</li>
-        <li className="typewriter">-Accesible</li>
-        <li className="typewriter">-SEO compatible</li>
-        <li className="typewriter">-Unique or Templated</li>
-        <li className="typewriter">-Interactive</li>
-        <li className="typewriter">-Dynamic</li>
-        <li className="typewriter">-Single or Multi Page</li>
-      </ul>
+      <div id="typewriterContainer">
+        <h1 className="typewriter" id="greetings">
+          Hi ,I'm <span id="name">Omer</span>
+        </h1>
+        <p id="whoIAm">
+          Frontend focused <span>Web Developer</span> who loves building
+          <span> efficent</span>, <span>clean</span> and
+          <span> scalable</span> projects
+        </p>
+
+        <h4 className="typewriter" id="projectQuality">
+          Project Qualities
+        </h4>
+        <ul id="qualityList">
+          <li className="typewriter">-Responsive</li>
+          <li className="typewriter">-Accessible</li>
+          <li className="typewriter">-SEO friendly</li>
+          <li className="typewriter">-Custom or Templated</li>
+          <li className="typewriter">-Interactive</li>
+
+          <li className="typewriter">-Single or MultiPage</li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Utils/animationDelay.js
+++ src/Utils/animationDelay.js
@@ -1,6 +1,27 @@
 const animationDelay = () => {
   const elements = document.querySelectorAll(".typewriter");
-  elements.forEach((e, index) => (e.style.animationDelay = `${index}.1s`));
+  elements.forEach((e, index) => {
+    let delay = 0;
+    for (let i = 1; i <= index; i++) {
+      delay += elements[i - 1].innerText.length;
+    }
+    let contentLength = e.innerText.length;
+
+    const typingKeyframes = `@keyframes typing-${index} {
+      from { width: 0; }
+      to { width: ${contentLength}ch; }
+    }`;
+
+    const style = document.createElement("style");
+    style.innerHTML = typingKeyframes;
+    document.head.appendChild(style);
+
+    e.style.animation = `typing-${index} ${contentLength / 20}s forwards,
+      blink-caret 0.5s forwards infinite,
+      matrix ${contentLength / 20}s steps(40, end)`;
+
+    e.style.animationDelay = `${delay / 20}s`;
+  });
 };
 
 export default animationDelay;
